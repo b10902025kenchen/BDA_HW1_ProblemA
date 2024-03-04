7dd2f4951e1a27415a5f4935f816f5ddf7e14f71
b10902025kenchen <100281527+b10902025kenchen@users.noreply.github.com>
2024-03-04 22:27:39 +0800

    Add files via upload

---
+++ download.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+mkdir videos
+cd videos
+wget 'https://docs.google.com/uc?export=download&id=1UDuu8pZGXYGebkdbTIQ3dXZSIggb_qoi' -O video1_16sec.mp4
+wget 'https://docs.google.com/uc?export=download&id=1fFpNZv_PqtcOCJmywl939ZY2VGT50ydh' -O video2_10sec.mp4
